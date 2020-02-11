5c4e76b69d79c803557b7fc60bd9126546673c1f
Galder Zamarreño <galder@zamarreno.com>
2020-02-11 11:49:21 +0100

    WIP

--- openshift/cache-service/jcache/src/main/java/org/infinispan/quickstart/openshift/cache/jcache/CreateCache.java
+++ openshift/cache-service/jcache/src/main/java/org/infinispan/quickstart/openshift/cache/jcache/CreateCache.java
@@ -6,10 +6,21 @@ import javax.cache.spi.*;
 
 public class CreateCache<cache> {
 
-  public static void main(String[] args) {
-    CacheManager cacheManager = Caching.getCachingProvider("org.infinispan.jcache.remote.JCachingProvider").getCacheManager();
-    Cache<String, String> cache = cacheManager.createCache("default",
-      new MutableConfiguration<String, String>());
-  }
+   private static final String KEY = "hello";
+   private static final String VALUE = "world";
+
+   public static void main(String[] args) {
+      CacheManager cacheManager = Caching.getCachingProvider("org.infinispan.jcache.remote.JCachingProvider").getCacheManager();
+      Cache<String, String> cache = cacheManager.getCache("default");
+
+      cache.put(KEY, VALUE);
+
+      System.out.printf("--- Retrieve key='%s' ---%n", KEY);
+      final String value = cache.get(KEY);
+
+      System.out.printf("--- Value is '%s' ---%n", value);
+
+      assert VALUE.equals(value) : "Expected value of '" + value + "' but was: " + value;
+   }
 
 }
